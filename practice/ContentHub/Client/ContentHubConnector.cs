fada6f107754090debd933051ea6a1556540a3ce
Chirag Khanna <ckhanna@horizontal.com>
2022-03-12 16:51:24 +0530

    initial commit

---
+++ practice/ContentHub/Client/ContentHubConnector.cs
@@ -0,0 +1,22 @@
+﻿using Stylelabs.M.Sdk.WebClient;
+using Stylelabs.M.Sdk.WebClient.Authentication;
+using System;
+
+namespace ContentHub.Client
+{
+    public static class ContentHubConnector
+    {
+        public static IWebMClient Client()
+        {
+            Uri endpoint = new Uri("https://hztlin03.stylelabsdemo.com");
+            OAuthPasswordGrant oauth = new OAuthPasswordGrant
+            {
+                ClientId = "client_id_chirag",
+                ClientSecret = "client_secret_chirag",
+                UserName = "chiragwebclientuser",
+                Password = "234qwe@#$QWE"
+            };
+            return MClientFactory.CreateMClient(endpoint, oauth);
+        }
+    }
+}
---
+++ practice/ContentHub/Program.cs
@@ -0,0 +1,23 @@
+﻿using ContentHub.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace ContentHub
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                await ContentHubConnector.Client().TestConnectionAsync();
+                Console.WriteLine("Connection is successful");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Unable to connect " + ex);
+            }
+            Console.ReadLine();
+        }
+    }
+}
